3e8e2a1292a0b1cdbe1205d871a5714ce12cf6e6
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-12-11 13:46:26 -0500

    added some more actions

--- actions.py
+++ actions.py
@@ -3,6 +3,7 @@ from enum import Enum
 from models import Api, Action, ApiUpAction, CheckApiAction, ApiDownAction
 
 
+# Maybe for future usage
 def on_check_api(api: Api) -> Action[CheckApiAction]:
     return Action(ApiActionTypes.api_up, CheckApiAction(api))
 
@@ -11,8 +12,8 @@ def on_api_up(api: Api) -> Action[ApiUpAction]:
     return Action(ApiActionTypes.api_up, ApiUpAction(api))
 
 
-def on_api_down(api: Api, error: str) -> Action[ApiDownAction]:
-    return Action(ApiActionTypes.api_up, ApiDownAction(api,error))
+def on_api_down(api: Api, msg: str) -> Action[ApiDownAction]:
+    return Action(ApiActionTypes.api_up, ApiDownAction(api, msg))
 
 
 class ApiActionTypes(str, Enum):
