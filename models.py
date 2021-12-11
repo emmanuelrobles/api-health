470d34a371786280ee752e2310ae9184549f2dd1
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-12-11 13:01:59 -0500

    added more models

--- models.py
+++ models.py
@@ -1,5 +1,32 @@
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
 class Api:
     def __init__(self, url: str, check_method: int, env: str):
         self.url = url
         self.check_method = url
         self.env = url
+
+
+class Action(Generic[T]):
+    def __init__(self, action_type: str, payload: T):
+        self.action_type = action_type
+        self.payload = payload
+
+
+class CheckApiAction:
+    def __init__(self, api: Api):
+        self.api = api
+
+
+class ApiUpAction:
+    def __init__(self, api: Api):
+        self.api = api
+
+
+class ApiDownAction:
+    def __init__(self, api: Api, error: str):
+        self.api = api
+        self.error = error
