c0d40e80994adeedef2f08713652916bce4e3f37
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-12-11 13:46:52 -0500

    added rx helper

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import List, Callable
 
-from models import Api
+from rx import operators, Observable
+
+from models import Api, Action
 
 
 def get_apis_from_json(json_data: dict) -> List[Api]:
@@ -11,3 +13,8 @@ def get_apis_from_json(json_data: dict) -> List[Api]:
         apis.append(Api(json_api['url'], json_api['check_method'], json_api['env']))
 
     return apis
+
+
+# Do action on action_type
+def do_action_when(action_type: str, callback: Callable[[Action], None]) -> Callable[[Observable], Observable]:
+    return operators.do_action(lambda action: callback(action) if action.action_type == action_type else None)
